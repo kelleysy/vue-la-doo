e2e39cafc89f5640f4ad9827b0a007609a6bf780
dillon burns <reach.dillon@gmail.com>
2017-07-25 14:28:55 -0400

    added item control (delete) button

---
+++ myapp/src/components/Item/Item_Control.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="deleteItem"> - </button>
+</template>
+
+<script>
+export default {
+
+  name: 'item-control',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+    deleteItem: function (event) {
+      console.log('Item Control: deleting')
+      console.log('event var - ')
+      console.log(event)
+      this.$emit('deleteItem')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
